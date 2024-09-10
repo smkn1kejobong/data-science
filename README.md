491fccc822ad62ec6dfb3339a75789f1bf3f6327
itnsapurbalingga <shinau21.personal@gmail.com>
2024-09-10 21:15:36 +0700

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# data-scientist
-This is sample data
+# Learn Data Science with Sample Data
+How to usage?
+
+Requirement :
+
+```bash
+NodeJS v18+
+Python3
+```
+
+```bash
+git clone https://github.com/smknkejobong/data-scientist
+cd api-data-faker
+npm install
+npm run test
+```
+
+Lets open your browser and type
+```bash
+http://localhost:3000/api/sales
+```
+Did you are found data?
+nothing?
+
+Lets generate data with this sample uri
+```bash
+http://localhost:3000/api/sales/generate/100
+```
+
+What next step?
+Explore more on your brain xD
---
+++ api-data-faker/main.js
@@ -0,0 +1,51 @@
+import express from 'express';
+import { fakerID_ID as faker} from '@faker-js/faker';
+const app = express();
+const port = 3000;
+
+const salesData = []
+
+// Endpoint untuk mendapatkan semua data penjualan
+app.get('/api/sales', (req, res) => {
+  res.json(salesData);
+});
+
+// Endpoint untuk generate data, return dari data akan disimpan ke salesData
+app.get('/api/sales/generate/:count', (req, res) => {
+  const count = parseInt(req.params.count, 10);
+  if (isNaN(count) || count <= 0) {
+    return res.status(400).json({ message: 'Jumlah harus berupa angka positif' });
+  }
+
+  const newSalesData = [];
+  const products = ["Motherboard","Processor","RAM","VGA Card","SPF+","Switch","Router"];
+  const customers = ["Rafli","Arif","Ichsan","Fajar","Zuhud","Fauzi"];
+  
+  for (let i = 0; i < count; i++) {
+    newSalesData.push({
+      id: faker.string.uuid(),
+      product: products[(Math.floor(Math.random() * products.length))],
+      price: faker.commerce.price({ min: 1000000, max: 10000000, dec: 0}),
+      quantity: faker.number.int({ min: 1, max: 30 }),
+      customer: customers[(Math.floor(Math.random() * customers.length))],
+      date: faker.date.between({ from: '2024-01-01T00:00:00.000Z', to: '2024-12-30T00:00:00.000Z' })
+    });
+  }
+
+  salesData.push(...newSalesData); // Menambahkan data baru ke salesData
+  return res.status(201).json({ message: `${count} Data telah ditambahkan ke Data Penjualan`})
+});
+
+// Endpoint untuk mendapatkan data penjualan berdasarkan ID
+app.get('/api/sales/:id', (req, res) => {
+  const sale = salesData.find(s => s.id === req.params.id);
+  if (sale) {
+    res.json(sale);
+  } else {
+    res.status(404).json({ message: 'Data penjualan tidak ditemukan' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server berjalan di http://localhost:${port}`);
+});
